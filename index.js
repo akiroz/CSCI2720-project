dc4da7f6454cf940a4e81c4c0b896e4b04d7d879
Akiroz <akiroz.vectis@gmail.com>
2017-12-08 21:51:36 +0000

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var querystring = require('querystring');
 var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 var fileUpload = require('express-fileupload');
 var csvStringify = require('csv-stringify');
 
@@ -18,6 +19,7 @@ var db = mysql.createConnection({
 // config app
 var app = express();
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
 
@@ -26,7 +28,7 @@ app.post('/login', (req, res) => {
   var username = req.body.username;
   var pHash = crypto.createHash('sha256');
   pHash.update(req.body.password);
-
+  
   db.query(
     'SELECT balance FROM user WHERE username = ? AND password = ?',
     [username, pHash.digest('hex')],
@@ -74,6 +76,7 @@ app.post('/item', (req, res) => {
     }
   );
 });
+
 // Retreve Item List ==============================================
 app.get('/item', (req, res) => {
   var { sortDesc, sortValue, page = 0 } = req.query;
@@ -81,43 +84,113 @@ app.get('/item', (req, res) => {
   var sortOrder = sortDesc ? 'DESC' : 'ASC';
   db.query(
     `SELECT id, title, value, qty, create_time FROM item ORDER BY ${sortField} ${sortOrder} LIMIT 10 OFFSET ?`,
-    [ page*10 ],
-    (err, results) => {
+    [ page*10 ], (err, items) => {
       if(err) throw err;
-      res.json(results);
+      db.query('SELECT COUNT(*) AS totalItems FROM item', (err, [{totalItems}]) => {
+        if(err) throw err;
+        res.json({ items, totalItems });
+      });
     }
   );
 });
+
 // Retreve Item ==============================================
 app.get('/item/:id', (req, res) => {
-  var { id } = req.param;
+  var { id } = req.params;
   db.query(
     'SELECT * FROM item WHERE id = ?', [ id ],
     (err, [item]) => {
       if(err) throw err;
-      res.json(item);
+      if(item) {
+        if(item.image) {
+          item.image = item.image.toString('base64');
+        }
+        item.tags = JSON.parse(item.tags);
+        res.json(item);
+      } else {
+        res.status(404).end();
+      }
     }
   );
 });
+
 // Update Item ==============================================
-app.put('/item/:id', (req, res) => {
-  var { id } = req.param;
-  var { title, description, image, value, qty, tags } = req.body;
+app.post('/item/:id', (req, res) => {
+  var { id } = req.params;
+  var { title, description, value, qty, tags } = req.body;
+  var {
+    image: {
+      data: imageData = null
+    } = {}
+  } = req.files || {};
+  var tagStr = JSON.stringify(tags.split(' '));
   db.query(
-    'UPDATE item SET title=? description=? image=? value=? qty=? tags=? WHERE id=?',
-    [ title, description, image, value, qty, tags, id ],
+    'UPDATE item SET title=?, description=?, image=?, value=?, qty=?, tags=? WHERE id=?',
+    [ title, description, imageData, value, qty, tagStr, id ],
     err => {
       if(err) throw err;
-      res.end();
+      // redirect back to previous page
+      res.redirect(req.get('Referer'));
     }
   );
 });
+
 // Delete Item ==============================================
 app.delete('/item/:id', (req, res) => {
-  var { id } = req.param;
+  var { id } = req.params;
   db.query('DELETE FROM item WHERE id=?', [id], err => {
     if(err) throw err;
     res.end();
+  });
+});
+
+// Redeem List =============================================
+app.get('/redeem', (req, res) => {
+  var { username } = req.cookies;
+  db.query(`
+    SELECT title, value, redeem_time
+    FROM redeem LEFT JOIN item ON redeem.item_id = item.id
+    WHERE username = ?
+  `, [username], (err, results) => {
+    if(err) throw err;
+    res.json(results);
+  });
+});
+
+// Redeem Item ==============================================
+app.post('/redeem', (req, res) => {
+  var { username } = req.cookies;
+  var { item } = req.body;
+  db.query(
+    'SELECT redeem_item(?,?) AS balance',
+    [ username, item ], (err, [{balance}]) => {
+      if(err) throw err;
+      if(balance < 0) {
+        res.status(400).end();
+      } else {
+        // redirect back to previous page
+        res.cookie('balance', balance);
+        res.redirect(req.get('Referer'));
+      }
+    }
+  );
+});
+
+// Export CSV
+app.get('/export', (_, res) => {
+  db.query(`
+    SELECT item.id AS id, title, value, redeem_time, username
+    FROM redeem LEFT JOIN item ON redeem.item_id = item.id
+  `, (err, results) => {
+    if(err) throw err;
+    var rows = results.map(r => [r.id, r.title, r.value, r.redeem_time.toString(), r.username]);
+    rows.unshift(['Item ID', 'Title', 'Value', 'Redeem Time', 'User']);
+    csvStringify(rows, (err, csv) => {
+      if(err) throw err;
+      res.set('Content-Disposition', 'attachment; filename=redeem.csv;');
+      res.set('Content-Type', 'text/csv');
+      res.send(csv);
+    });
   });
 });
 
@@ -135,11 +208,13 @@ var initSQL = `
     tags        TEXT, ${/* JSON array */''}
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );
+  
   CREATE TABLE IF NOT EXISTS user (
     username  VARCHAR(64) PRIMARY KEY,
     password  TEXT,
     balance   INT
   );
+  
   CREATE TABLE IF NOT EXISTS redeem (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     username    VARCHAR(64),
@@ -148,6 +223,32 @@ var initSQL = `
     FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (item_id) REFERENCES item(id)
   );
+  
+  DROP FUNCTION IF EXISTS redeem_item;
+  CREATE FUNCTION redeem_item(
+    user VARCHAR(64),
+    item INT
+  ) RETURNS INT BEGIN
+    DECLARE cost INT;
+    DECLARE stock INT;
+    DECLARE cur_balance INT;
+    DECLARE new_balance INT;
+    
+    SELECT value, qty INTO cost, stock FROM item WHERE id = item;
+    SELECT balance INTO cur_balance FROM user WHERE username = user;
+    
+    IF cur_balance < cost OR stock < 1 THEN
+      RETURN -1;
+    END IF;
+    
+    UPDATE user SET balance = balance - cost WHERE username = user;
+    UPDATE item SET qty = qty - 1 WHERE id = item;
+    INSERT INTO redeem (username, item_id) VALUES (user, item);
+    
+    SELECT balance INTO new_balance FROM user WHERE username = user;
+    RETURN new_balance;
+  END;
+  
   INSERT INTO user VALUES (
     'admin', ${/* password: admin */''}
     '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
